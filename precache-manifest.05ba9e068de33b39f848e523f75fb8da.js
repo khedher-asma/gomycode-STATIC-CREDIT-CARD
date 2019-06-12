fc3acba1ecace49d8f3280924ff5b7d1f98abd6d
khedher-asma <khedherasma90@gmail.com>
2019-06-12 14:28:27 +0100

    Updates

---
+++ precache-manifest.05ba9e068de33b39f848e523f75fb8da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e283236d23a7a39463b139d8875d04ca",
+    "url": "/STATIC-CREDIT-CARD/index.html"
+  },
+  {
+    "revision": "19c9655e67620452daba",
+    "url": "/STATIC-CREDIT-CARD/static/css/main.b239e9c9.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/STATIC-CREDIT-CARD/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "19c9655e67620452daba",
+    "url": "/STATIC-CREDIT-CARD/static/js/main.5d96afb4.chunk.js"
+  },
+  {
+    "revision": "f8f5b521ca3a36c49f54",
+    "url": "/STATIC-CREDIT-CARD/static/js/runtime~main.c0002bef.js"
+  },
+  {
+    "revision": "e31fdcb921fa62efac61ed02a93f4d0d",
+    "url": "/STATIC-CREDIT-CARD/static/media/earth.e31fdcb9.png"
+  },
+  {
+    "revision": "4ba08830bdba694f64404ddd6dc2c54a",
+    "url": "/STATIC-CREDIT-CARD/static/media/puce.4ba08830.png"
+  },
+  {
+    "revision": "f492aed39fb29088f764065848390c8a",
+    "url": "/STATIC-CREDIT-CARD/static/media/visaCard.f492aed3.png"
+  }
+]);
